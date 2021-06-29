f0c421dbe9ba61ae4c6c9ad60b14bfb96fa866a7
ValaA88t <vala.arefi@gmail.com>
2021-06-29 19:10:33 +0200

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <a href="/">Home Page</a>
+      <a href="about">About</a>
+      <a href="postCards">Post Cards</a>
+      <div className={styles.cart}>
+        <img className={styles.icon} src="../shoping-card.jpeg" />
+      </div>
+    </header>
+  );
+}
