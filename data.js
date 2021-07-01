218b6bedcc19fb9186edd19166383c7cb2db61a2
ValaA88t <vala.arefi@gmail.com>
2021-07-01 23:04:46 +0200

    Add table

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    title: 'birthday1',
+    url: '/birthdayPic/happy_birthday_card_1.jpeg',
+    tag: 'birthday',
+  },
+  {
+    title: 'birthday1',
+    url: '/birthdayPic/happy_birthday_card_1.jpeg',
+    tag: 'birthday',
+  },
+  {
+    title: 'birthday1',
+    url: '/birthdayPic/happy_birthday_card_1.jpeg',
+    tag: 'birthday',
+  },
+  {
+    title: 'birthday1',
+    url: '/birthdayPic/happy_birthday_card_1.jpeg',
+    tag: 'birthday',
+  },
+  {
+    title: 'birthday1',
+    url: '/birthdayPic/happy_birthday_card_1.jpeg',
+    tag: 'birthday',
+  },
+];
+
+export default data;
--- pages/postCards/index.js
+++ pages/postCards/index.js
@@ -1,5 +1,17 @@
 import Header from '../../components/Header/Header';
+import data from '../../data';
 
 export default function PostCards() {
-  return <div>Hello</div>;
+  console.log(data);
+  return (
+    <div>
+      <Header />
+      {data.map((card) => (
+        <div key={card.title}>
+          <div>{card.title}</div>
+          <img src={card.url}></img>
+        </div>
+      ))}
+    </div>
+  );
 }
