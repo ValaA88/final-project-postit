59ffb538f52a43185db9c97b5d18b7ef4d502f15
ValaA88t <vala.arefi@gmail.com>
2021-07-01 23:32:08 +0200

    Add culomn

--- data.js
+++ data.js
@@ -1,28 +1,48 @@
 const data = [
   {
-    title: 'birthday1',
-    url: '/birthdayPic/happy_birthday_card_1.jpeg',
-    tag: 'birthday',
+    category: 'birthday',
+    items: [
+      {
+        title: 'birthday1',
+        url: '/birthdayPic/happy_birthday_card_1.jpeg',
+        tag: 'birthday',
+      },
+      {
+        title: 'birthday1',
+        url: '/birthdayPic/happy_birthday_card_1.jpeg',
+        tag: 'birthday',
+      },
+      {
+        title: 'birthday1',
+        url: '/birthdayPic/happy_birthday_card_1.jpeg',
+        tag: 'birthday',
+      },
+      {
+        title: 'birthday1',
+        url: '/birthdayPic/happy_birthday_card_1.jpeg',
+        tag: 'birthday',
+      },
+    ],
   },
   {
-    title: 'birthday1',
-    url: '/birthdayPic/happy_birthday_card_1.jpeg',
-    tag: 'birthday',
-  },
-  {
-    title: 'birthday1',
-    url: '/birthdayPic/happy_birthday_card_1.jpeg',
-    tag: 'birthday',
-  },
-  {
-    title: 'birthday1',
-    url: '/birthdayPic/happy_birthday_card_1.jpeg',
-    tag: 'birthday',
-  },
-  {
-    title: 'birthday1',
-    url: '/birthdayPic/happy_birthday_card_1.jpeg',
-    tag: 'birthday',
+    category: 'christmas',
+    items: [
+      {
+        title: 'christmas1',
+        url: '/christmasPic/merry_christmas_postcard_1.jpeg',
+        tag: 'christmas',
+      },
+      {
+        title: 'christmas1',
+        url: '/christmasPic/merry_christmas_postcard_1.jpeg',
+        tag: 'christmas',
+      },
+      {
+        title: 'christmas1',
+        url: '/christmasPic/merry_christmas_postcard_1.jpeg',
+        tag: 'christmas',
+      },
+    ],
   },
 ];
 
--- pages/postCards/index.js
+++ pages/postCards/index.js
@@ -1,17 +1,22 @@
 import Header from '../../components/Header/Header';
 import data from '../../data';
+import styles from './postcards.module.css';
 
 export default function PostCards() {
   console.log(data);
   return (
     <div>
       <Header />
-      {data.map((card) => (
-        <div key={card.title}>
-          <div>{card.title}</div>
-          <img src={card.url}></img>
-        </div>
-      ))}
+      <div>
+        {data.map((card) => (
+          <div key={card.title}>
+            <div>{card.category}</div>
+            {card.items.map((item) => (
+              <img src={item.url}></img>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
